0009e30fafb02a48d4b824f006433b7b62084e80
snehtigerpatel <snehp@ymail.com>
2025-12-29 21:21:39 +0000

    chore: add base config files

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,ts,jsx,tsx,md}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0f172a',
+        accent: '#06b6d4'
+      }
+    }
+  },
+  plugins: []
+};
